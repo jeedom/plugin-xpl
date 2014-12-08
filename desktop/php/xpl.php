4feb87053739f43113b9d8e4ad68899c214fd514
loic <lgevrey@oxya.com>
2014-12-08 17:23:16 +0100

    Improvement display page

--- desktop/php/xpl.php
+++ desktop/php/xpl.php
@@ -7,6 +7,7 @@ include_file('core', 'xpl', 'config', 'xpl');
 include_file('core', 'xpl', 'class', 'xpl');
 
 sendVarToJS('eqType', 'xpl');
+$eqLogics = eqLogic::byType('xpl');
 ?>
 
 <div class="row row-overflow">
@@ -16,12 +17,35 @@ sendVarToJS('eqType', 'xpl');
                 <a class="btn btn-default eqLogicAction" style="width : 100%;margin-top : 5px;margin-bottom: 5px;" data-action="add"><i class="fa fa-plus-circle"></i> {{Ajouter un équipement}}</a>
                 <li class="filter" style="margin-bottom: 5px;"><input class="filter form-control input-sm" placeholder="{{Rechercher}}" style="width: 100%"/></li>
                 <?php
-                foreach (eqLogic::byType('xpl') as $eqLogic) {
+                foreach ($eqLogics as $eqLogic) {
                     echo '<li class="cursor li_eqLogic" data-eqLogic_id="' . $eqLogic->getId() . '"><a>' . $eqLogic->getHumanName(true) . '</a></li>';
                 }
                 ?>
             </ul>
         </div>
+    </div>
+    
+     <div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">
+        <legend>{{Mes équipements xPL}}
+        </legend>
+        <?php
+        if (count($eqLogics) == 0) {
+            echo "<br/><br/><br/><center><span style='color:#767676;font-size:1.2em;font-weight: bold;'>{{Vous n'avez pas encore d'équipement xPL, cliquez sur Ajouter un équipement pour commencer}}</span></center>";
+        } else {
+            ?>
+            <div class="eqLogicThumbnailContainer">
+                <?php
+                foreach ($eqLogics as $eqLogic) {
+                    echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >';
+                    echo "<center>";
+                    echo '<img src="plugins/xpl/doc/images/xpl_icon.png" height="105" width="95" />';
+                    echo "</center>";
+                    echo '<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;"><center>' . $eqLogic->getHumanName(true, true) . '</center></span>';
+                    echo '</div>';
+                }
+                ?>
+            </div>
+        <?php } ?>
     </div>
 
     <div class="col-lg-10 col-md-9 col-sm-8 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
@@ -33,7 +57,7 @@ sendVarToJS('eqType', 'xpl');
         ?>
         <form class="form-horizontal">
             <fieldset>
-                <legend>{{Général}}</legend>
+                <legend><i class="fa fa-arrow-circle-left eqLogicAction cursor" data-action="returnToThumbnailDisplay"></i> {{Général}}</legend>
                 <div class="form-group">
                     <label class="col-sm-3 control-label">{{Nom de l'équipement xPL}}</label>
                     <div class="col-sm-3">
