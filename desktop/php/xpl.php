6e9cf90da7410915b5a1fbfa371b3b12b1f67627
Loic <loicgevrey@gmail.com>
2015-08-30 17:41:04 +0200

    UI improvement

--- desktop/php/xpl.php
+++ desktop/php/xpl.php
@@ -18,7 +18,8 @@ $eqLogics = eqLogic::byType('xpl');
                 <li class="filter" style="margin-bottom: 5px;"><input class="filter form-control input-sm" placeholder="{{Rechercher}}" style="width: 100%"/></li>
                 <?php
 foreach ($eqLogics as $eqLogic) {
-	echo '<li class="cursor li_eqLogic" data-eqLogic_id="' . $eqLogic->getId() . '"><a>' . $eqLogic->getHumanName(true) . '</a></li>';
+	$opacity = ($eqLogic->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogic:style:noactive');
+	echo '<li class="cursor li_eqLogic" data-eqLogic_id="' . $eqLogic->getId() . '" style="' . $opacity . '"><a>' . $eqLogic->getHumanName(true) . '</a></li>';
 }
 ?>
             </ul>
@@ -36,10 +37,7 @@ if (count($eqLogics) == 0) {
             <div class="eqLogicThumbnailContainer">
                 <?php
 foreach ($eqLogics as $eqLogic) {
-		$opacity = '';
-		if ($eqLogic->getIsEnable() != 1) {
-			$opacity = 'opacity:0.3;';
-		}
+		$opacity = ($eqLogic->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogic:style:noactive');
 		echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;' . $opacity . '" >';
 		echo "<center>";
 		echo '<img src="plugins/xpl/doc/images/xpl_icon.png" height="105" width="95" />';
